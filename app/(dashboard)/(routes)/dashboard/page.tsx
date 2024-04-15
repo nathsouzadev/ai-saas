05b5129d9904d935cf6d749903ee9f8826e3e116
Nathally Souza <nathallyccd@gmail.com>
2024-04-15 10:06:56 -0300

    [FEAT]create sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from '@clerk/nextjs';
-
 export default function DashboardPage() {
   return (
     <div>
       <p>Dashboard</p>
-      <UserButton afterSignOutUrl='/'/>
     </div>
   );
 }
